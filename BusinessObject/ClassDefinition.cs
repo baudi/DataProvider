352b468d179f98b217060fc33ec91da1c70409ff
Baudilio Huanca <klb.imfo642@gmail.com>
2013-02-02 02:05:14 -0400

    Revert "Clean"
    This reverts commit ecf2e888d384481ad817bc4ea24c5386068ee072.

--- BusinessObject/ClassDefinition.cs
+++ BusinessObject/ClassDefinition.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace BusinessObject
 {
     public class ClassDefinition
     {
-        public long ClassDefinitionId { get; set; }
+        public int ClassDefinitionId { get; set; }
         public string Name { get; set; }
 
         public virtual ICollection<AttributeDefinition> Attributes { get; set; }
         public virtual ICollection<ClassInstance> ClassInstances { get; set; }
+
+        
     }
 }
